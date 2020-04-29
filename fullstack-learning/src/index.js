1b06750950f947eea82dae9e9585c69e963c33c7
dilippoudel <dilippoudel@Dilip-MBP.Home>
2020-04-29 19:35:59 +0300

    Stastics component added : step-3 ==> 1.8

--- fullstack-learning/src/index.js
+++ fullstack-learning/src/index.js
@@ -1,48 +1,55 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-const Button = ({eventHandler, text}) => {
-return(
-    <button onClick={eventHandler}>{text}</button>
-)
-}
+// Button Component
 
+const Button = ({ eventHandler, text }) => {
+  return <button onClick={eventHandler}>{text}</button>;
+};
 
+// Stastics component
+
+const Stastics = ({ good, neutral, bad, all }) => {
+  return (
+    <>
+      <h3>Stastifs</h3>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {all}</p>
+      <p>average {(good - bad) / all}</p>
+      <p>positive {(good / all) * 100}</p>
+    </>
+  );
+};
+
+// Parent- App Component
 const App = () => {
-const [good, setGood] = useState(0);
-const [bad, setBad] = useState(0)
-const [neutral, setNeutral] = useState(0);
-const [all, setAll] = useState(0);
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [all, setAll] = useState(0);
 
-const goodFeedback = () => {
-    setGood(good+1)
-    setAll(all+1)
-}
-const neutralFeedback = () => {
-    setNeutral(neutral+1)
-    setAll(all+1)
-}
-const badFeedback = () => {
-    setBad(bad+1)
-    setAll(all+1)
-}
-return(
+  const goodFeedback = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+  };
+  const neutralFeedback = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+  };
+  const badFeedback = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+  };
+  return (
     <div>
-        <h2>give feedback</h2>
-        <Button eventHandler={goodFeedback} text="good"/>
-        <Button eventHandler={neutralFeedback} text="neutral"/>
-        <Button eventHandler={badFeedback} text="bad"/>
-        <h3>Stastifs</h3>
-        <p>Good {good}</p>
-        <p>Neutral {neutral}</p>
-        <p>Bad {bad}</p>
-        <p>All {all}</p>
-        <p>average {(good-bad)/all}</p>
-        <p>positive {(good / all) * 100}</p>
+      <h2>give feedback</h2>
+      <Button eventHandler={goodFeedback} text="good" />
+      <Button eventHandler={neutralFeedback} text="neutral" />
+      <Button eventHandler={badFeedback} text="bad" />
+      <Stastics good={good} bad={bad} neutral={neutral} all={all} />
     </div>
-)
-}
-ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
-)
+  );
+};
+ReactDOM.render(<App />, document.getElementById("root"));
